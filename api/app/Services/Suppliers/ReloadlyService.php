75d711bdc5b2d9e955eef88ad0ce1303195597d9
Muhammad Aliyan <aliyantarar4@gmail.com>
2026-03-21 00:14:29 +0500

    Fix missing Log facade import in ReloadlyService

--- api/app/Services/Suppliers/ReloadlyService.php
+++ api/app/Services/Suppliers/ReloadlyService.php
@@ -7,6 +7,7 @@ use App\Models\Order;
 use App\Models\SupplierConnection;
 use Illuminate\Support\Facades\Cache;
 use Illuminate\Support\Facades\Http;
+use Illuminate\Support\Facades\Log;
 use Exception;
 
 class ReloadlyService implements SupplierServiceInterface
